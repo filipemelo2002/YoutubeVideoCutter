828e120d5a48b630fad834e5568da140c5601bbf
Filipe Melo <filipemelo032@gmail.com>
2021-02-02 20:32:04 -0300

    Removed URL

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import Downloader from './modules/Download';
 import Cutter from './modules/Cutter';
 (async () => {
-  const downloader = new Downloader(
-    'https://www.youtube.com/watch?v=QNKFnYNsdL8',
-  );
+  const downloader = new Downloader('');
   const videoInfo = await downloader.exec();
   const cutter = new Cutter();
   await cutter.exec({
